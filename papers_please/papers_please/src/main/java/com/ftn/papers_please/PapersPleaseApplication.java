9ec94085b704cba7aa6af2f5ae25d0e8ff47e4ce
ivanmihajlov <mihajlov.ivan94@gmail.com>
2020-08-30 22:28:01 +0200

    Initial backend commit

---
+++ papers_please/papers_please/src/main/java/com/ftn/papers_please/PapersPleaseApplication.java
@@ -0,0 +1,13 @@
+package com.ftn.papers_please;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapersPleaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PapersPleaseApplication.class, args);
+	}
+
+}
